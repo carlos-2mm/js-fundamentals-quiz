756e15d4286da2f39bd3a9f9c3607aa01ef50e46
Carlos Macias <cimm21187@gmail.com>
2023-07-30 01:49:57 -0400

    Implemented a feature that hides the sections for questions, quiz over, and high scores until the start button is clicked. Added 'questions' and 'answers' variables in the JavaScript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,48 @@ function startTimer() {
       timerElement.innerText = 'time: ' + (timerValue) + ' seconds';
     }
   }, 1000);
+
+  // Show the container-q, container-qo, and container hs elements
+    document.querySelector('.container-q').style.display = 'block';
+    document.querySelector('.container-qo').style.display = 'block';
+    document.querySelector('.container-hs').style.display = 'block';
 }
 
 // event listener to start the timer when the "start quiz" button is clicked
 var startButton = document.getElementById('start');
 startButton.addEventListener('click', startTimer);
 
+
+// array of quiz questions and their corresponding answer options
+var quizQuestions = [
+    {
+      question: 'commonly used data types DO NOT include:',
+      answers: ['strings', 'booleans', 'alerts', 'numbers'],
+      correctAnswer: "alerts"
+    },
+
+    {
+        question: 'the condition in an if/else statement is enclosed with ___________.',
+        answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+        correctAnswer: 'parenthesis'
+      },
+
+      {
+        question: 'arrays in JavaScript can be used to store ___________.',
+        answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        correctAnswer: 'all of the above'
+      },
+   
+      {
+        question: 'strings values must be enclosed within ___________ when being assigned to variables',
+        answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+        correctAnswer: 'commas'
+      },
+
+      {
+        question: 'a very useful tool used during development and debugging for printing content to the debugger is',
+        answers: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+        correctAnswer: 'console.log'
+      },
+  ];
+  
