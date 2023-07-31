32fc297a2e83a8313419b1d4f11254ef23f50d6a
Carlos Macias <cimm21187@gmail.com>
2023-07-30 21:41:00 -0400

    This commit adds the necessary JavaScript code to implement a quiz. The code includes functions to display questions, answer options, check selected answers, and show responses.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,8 @@ function startTimer() {
 
   // Show the container-q, container-qo, and container hs elements
     document.querySelector('.container-q').style.display = 'block';
-    document.querySelector('.container-qo').style.display = 'block';
-    document.querySelector('.container-hs').style.display = 'block';
+    document.querySelector('.container-qo').style.display = 'none';
+    document.querySelector('.container-hs').style.display = 'none';
 }
 
 // event listener to start the timer when the "start quiz" button is clicked
@@ -62,4 +62,65 @@ var quizQuestions = [
         correctAnswer: 'console.log'
       },
   ];
+
+  var currentQuestionIndex = 0;
   
+  // function to display the current question and its answer options
+  function displayQuestion() {
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    var questionTextElement = document.getElementById('questionText');
+    var answerOptionsElement = document.getElementById('answerOptions');
+  
+    // display the question text
+    questionTextElement.textContent = currentQuestion.question;
+  
+    answerOptionsElement.innerHTML = '';
+  
+    // create the answer option buttons
+    currentQuestion.answers.forEach(function (answer) {
+      var answerButton = document.createElement('button');
+      answerButton.textContent = answer;
+      answerButton.addEventListener('click', function () {
+        checkAnswer(answer);
+      });
+      answerOptionsElement.appendChild(answerButton);
+    });
+  }
+  
+  // function to check the selected answer and show the response
+  function checkAnswer(selectedAnswer) {
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    var responseElement = document.getElementById('response');
+  
+    // check if the selected answer is correct
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      responseElement.textContent = 'correct!';
+    } else {
+      responseElement.textContent = 'wrong!';
+      
+      // add function here for penalties on incorrect answers
+    }
+  
+    // move to the next question
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizQuestions.length) {
+      setTimeout(function () {
+        responseElement.textContent = '';
+        displayQuestion();
+      }, 1000);
+    } else {
+      // display the quiz over message
+      setTimeout(function () {
+        document.querySelector('.container-q').style.display = 'none';
+        document.querySelector('.container-qo').style.display = 'block';
+      }, 1000);
+    }
+  }
+  
+  // event listener to start the quiz
+  var startButton = document.getElementById('start');
+  startButton.addEventListener('click', function () {
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container-q').style.display = 'block';
+    displayQuestion();
+  });
